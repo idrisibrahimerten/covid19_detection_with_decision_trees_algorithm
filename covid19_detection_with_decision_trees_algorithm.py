4f873bebd6f4ea9515105bc0921e1ae605d71009
İdris İbrahim ERTEN <82749259+idrisibrahimerten@users.noreply.github.com>
2022-04-06 03:09:58 +0300

    Add files via upload

---
+++ covid19_detection_with_decision_trees_algorithm.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 20:05:33 2022
+
+@author: IdrisIbrahimERTEN
+"""
+
+import numpy as np
+from PIL import Image as img
+import os
+import pandas as pd
+
+covidli = "yolu_veriniz/COVID"
+covidli_olmayan = "yolu_veriniz/non-COVID"
+
+def dosya(yol):
+    return [os.path.join(yol, f) for f in os.listdir(yol)]
+
+def veri_donusturme(klasor_adi,sinif_adi):
+
+    goruntuler=dosya(klasor_adi)
+    
+    goruntu_sinif=[]
+    for goruntu in goruntuler:
+        goruntu_oku= img.open(goruntu).convert('L')
+        gorunu_boyutlandirma=goruntu_oku.resize((28,28))
+        goruntu_donusturme=np.array(gorunu_boyutlandirma).flatten()
+        if sinif_adi=="covidli":
+            veriler=np.append (goruntu_donusturme, [0])
+            
+        elif sinif_adi=="covid_olmayan":
+            veriler=np.append (goruntu_donusturme, [1])
+            
+        else:
+            continue
+        goruntu_sinif.append(veriler)
+
+    return goruntu_sinif
+
+covidli_veri=veri_donusturme(covidli,"covidli")
+covidli_df=pd.DataFrame(covidli_veri)
+
+covidli_olmayan_veri=veri_donusturme(covidli_olmayan,"covid_olmayan")
+covidli_olmayan_df=pd.DataFrame(covidli_olmayan_veri)
+
+tum_veri= pd.concat([covidli_df, covidli_olmayan_df])
+
+tum_veri = pd.concat([covidli_df, covidli_olmayan_df])
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+
+Giris = np.array(tum_veri)[:,:784]
+Cikis = np.array(tum_veri)[:,784]
+
+Giris_train, Giris_test, Cikis_train, Cikis_test = train_test_split(Giris, Cikis, test_size = 0.2, random_state=1)
+
+model = DecisionTreeClassifier()
+clf = model.fit(Giris_train, Cikis_train)
+Cikis_pred = clf.predict(Giris_test)
+
+print("Doğruluk Oranı : " , metrics.accuracy_score(Cikis_test, Cikis_pred))
+
+import matplotlib.pyplot as cizim
+ypo, dpo, threshold = metrics.roc_curve(Cikis_test, Cikis_pred)
+roc_auc = metrics.auc(ypo, dpo)
+cizim.title("ROC EĞRİSİ")
+cizim.plot(ypo, dpo, 'b', label = 'AUC = %0.2f' % roc_auc)
+cizim.legend(loc = "lower right")
+cizim.plot([0, 1], [0, 1], 'r--')
+cizim.xlim([0, 1])
+cizim.ylim([0, 1])
+cizim.ylabel("Doğru Pozitif Oranı")
+cizim.xlabel("Yanlış Pozitif Oranı")
+cizim.show()
